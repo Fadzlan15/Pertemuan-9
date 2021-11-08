7cc58037b7cae98d397ab1fc88037fdff5142d75
unknown <fadzlanfairus22@gmail.com>
2021-11-08 16:08:01 +0700

    Menambahkan commit folder

---
+++ pendaftaran_siswa/form-daftar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulir Pendaftaran Siswa Baru</title>
+</head>
+
+<body>
+    <header>
+        <h3>Formulir Pendaftaran Siswa Baru</h3>
+    </header>
+
+    <form action="proses-pendaftaran.php" method="POST">
+
+        <fieldset>
+
+        <p>
+            <label for="nama">Nama: </label>
+            <input type="text" name="nama" placeholder="nama lengkap" />
+        </p>
+        <p>
+            <label for="alamat">Alamat: </label>
+            <textarea name="alamat"></textarea>
+        </p>
+        <p>
+            <label for="sekolah_asal">Sekolah Asal: </label>
+            <input type="text" name="sekolah_asal" placeholder="nama sekolah" />
+        </p>
+        <p>
+            <input type="submit" value="Daftar" name="daftar" />
+        </p>
+
+        </fieldset>
+
+    </form>
+
+    </body>
+</html>
---
+++ pendaftaran_siswa/proses-pendaftaran.php
@@ -0,0 +1,31 @@
+<?php
+
+include("config.php");
+
+// cek apakah tombol daftar sudah diklik atau blum?
+if(isset($_POST['daftar'])){
+
+    // ambil data dari formulir
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $sekolah = $_POST['sekolah_asal'];
+
+    // buat query
+    $sql = "INSERT INTO calon_siswa (nama, alamat, sekolah_asal) VALUE ('$nama', '$alamat', '$sekolah')";
+    $query = mysqli_query($db, $sql);
+
+    // apakah query simpan berhasil?
+    if( $query ) {
+        // kalau berhasil alihkan ke halaman index.php dengan status=sukses
+        header('Location: index.php?status=sukses');
+    } else {
+        // kalau gagal alihkan ke halaman indek.php dengan status=gagal
+        header('Location: index.php?status=gagal');
+    }
+
+
+} else {
+    die("Akses dilarang...");
+}
+
+?>
